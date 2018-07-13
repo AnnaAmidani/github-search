eb5394c46fe17262a42edf1e9771b6351830c3a2
Anna Amidani <anna.amidani@soprasteria.com>
2018-07-13 22:47:16 +0100

    add routing to repos results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReposComponent } from './repos/repos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: ReposComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
