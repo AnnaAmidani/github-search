9d83f8ea672cb86796c61879e274b6206cf64338
Anna Amidani <anna.amidani@soprasteria.com>
2018-07-14 14:05:59 +0100

    conf routing to search and re defined home and repos routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReposComponent } from './components/repos/repos.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: ReposComponent
+  },
+  {
+    path: 'repos',
+    component: ReposComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
   }
 ];
 
