07170ee9164ce7877317a51b61b96b5551c0bd2a
Anna Amidani <anna.amidani@soprasteria.com>
2018-07-15 15:06:25 +0100

    Enhancement of Repo component

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SearchService } from '../../services/search.service';
-import { Repo } from '../model/Repo';
+import { Repo } from '../../model/Repo';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-repos',
+  providers: [ SearchService ],
   templateUrl: './repos.component.html',
   styleUrls: ['./repos.component.scss']
 })
@@ -16,9 +17,13 @@ export class ReposComponent implements OnInit {
   constructor(private searchdata: SearchService) { }
 
   ngOnInit() {
-    this.searchdata.getRepos().subscribe(
-      data => this.repos$ = data['items']
-    );
+    this.getTrendyRepos();
   }
+
+  getTrendyRepos(): void {
+    this.searchdata.findRepos('trendy+language:java')
+      .subscribe(repos => this.repos$ = repos['items']);
+  }
+
 
 }
